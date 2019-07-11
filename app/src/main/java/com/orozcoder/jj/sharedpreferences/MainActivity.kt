242f55558d212c8ddec82c8e14935a218eae4110
JJ <jj.orozcolopez.13@gmail.com>
2019-07-11 10:48:55 -0600

    Added activity, layout and class that allow write, read and delete a preference.

--- app/src/main/java/com/orozcoder/jj/sharedpreferences/MainActivity.kt
+++ app/src/main/java/com/orozcoder/jj/sharedpreferences/MainActivity.kt
@@ -2,11 +2,37 @@ package com.orozcoder.jj.sharedpreferences
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sp = SharedPreferences(this)
+
+        btnLeer.setOnClickListener{
+            if(sp.leer() == null){
+                etPreferencias.setText("No se encontró registro.")
+            }
+            else{
+                etPreferencias.setText(sp.leer())
+            }
+        }
+
+        btnEscribir.setOnClickListener{
+            sp.escribir(etPreferencias.text.toString())
+            etPreferencias.setText("")
+        }
+
+        btnBorrar.setOnClickListener{
+            sp.borrar()
+            etPreferencias.setText("")
+        }
+
+
     }
+
 }
